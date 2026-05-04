212ac7a03ecf6b9d573357ebe83776c11733cdfa
Vaibhav Jha <vaibhavjha100@gmail.com>
2026-05-04 20:30:51 +1000

    Add functionality to evaluate and store unique stock pairs in spread.py. This includes applying the eval function to the 'pairs' column and creating a set to hold all unique pairs, enhancing data integrity for subsequent analysis.

--- spread.py
+++ spread.py
@@ -18,7 +18,12 @@ import pickle
 
 # Load pairs.csv
 pairs = pd.read_csv('pairs.csv', index_col=0, parse_dates=[0])
+pairs['pairs'] = pairs['pairs'].apply(eval)
 
 # Load nifty500.csv
 nifty500 = pd.read_csv('nifty500.csv', index_col=0, parse_dates=[0])
 
+all_pairs = set()
+for pair in pairs['pairs']:
+    # Add pair to all_pairs set
+    all_pairs.add(tuple(pair))
